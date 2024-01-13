119db57370bb5c67f060254f8a92493c298c82f7
Isala Piyarisi <mail@isala.me>
2024-01-13 17:21:52 +0530

    Completed eBPF side

--- scale-to-zero/src/main.rs
+++ scale-to-zero/src/main.rs
@@ -1,15 +1,25 @@
-use anyhow::Context;
-use aya::programs::{Xdp, XdpFlags};
-use aya::{include_bytes_aligned, Bpf};
+use aya::{
+    include_bytes_aligned,
+    maps::{perf::AsyncPerfEventArray, HashMap},
+    programs::{Xdp, XdpFlags},
+    util::online_cpus,
+    Bpf,
+};
 use aya_log::BpfLogger;
 use clap::Parser;
-use log::{info, warn, debug};
-use tokio::signal;
+use log::{info, warn};
+use std::net::Ipv4Addr;
+use scale_to_zero_common::PacketLog;
+use bytes::BytesMut;
+use tokio::{signal, task};
 
 #[derive(Debug, Parser)]
 struct Opt {
     #[clap(short, long, default_value = "eth0")]
     iface: String,
+
+    #[clap(short, long, default_value = "default")]
+    attach_mode: String,
 }
 
 #[tokio::main]
@@ -17,17 +27,6 @@ async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
 
     env_logger::init();
-
-    // Bump the memlock rlimit. This is needed for older kernels that don't use the
-    // new memcg based accounting, see https://lwn.net/Articles/837122/
-    let rlim = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
-    };
-    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    if ret != 0 {
-        debug!("remove limit on locked memory failed, ret is: {}", ret);
-    }
 
     // This will include your eBPF object file as raw bytes at compile-time and load it at
     // runtime. This approach is recommended for most real-world use cases. If you would
@@ -45,10 +44,52 @@ async fn main() -> Result<(), anyhow::Error> {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {}", e);
     }
-    let program: &mut Xdp = bpf.program_mut("scale_to_zero").unwrap().try_into()?;
+    let program: &mut Xdp =
+        bpf.program_mut("xdp_scale_to_zero_fw").unwrap().try_into()?;
     program.load()?;
-    program.attach(&opt.iface, XdpFlags::default())
-        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+
+    let attach_mode = opt.attach_mode.as_str();
+
+    if attach_mode == "default" {
+        program.attach(&opt.iface, XdpFlags::default())?;
+    } else if attach_mode == "skb" {
+        program.attach(&opt.iface, XdpFlags::SKB_MODE)?;
+    } else if attach_mode == "hw" {
+        program.attach(&opt.iface, XdpFlags::HW_MODE)?;
+    } else {
+        panic!("Unknown attach mode: {}", attach_mode);
+    }
+
+    let mut scalable_service_list: HashMap<_, u32, u32> =
+        HashMap::try_from(bpf.map_mut("SERVICE_LIST").unwrap())?;
+    let block_addr: u32 = Ipv4Addr::new(1, 1, 1, 1).try_into()?;
+    scalable_service_list.insert(block_addr, 0, 0)?;
+
+
+    let mut perf_array =
+        AsyncPerfEventArray::try_from(bpf.take_map("SCALE_REQUESTS").unwrap())?;
+
+    for cpu_id in online_cpus()? {
+        let mut buf = perf_array.open(cpu_id, None)?;
+
+        task::spawn(async move {
+            let mut buffers = (0..10)
+                .map(|_| BytesMut::with_capacity(1024))
+                .collect::<Vec<_>>();
+
+            loop {
+                let events = buf.read_events(&mut buffers).await.unwrap();
+                for buf in buffers.iter_mut().take(events.read) {
+                    let ptr = buf.as_ptr() as *const PacketLog;
+                    let data = unsafe { ptr.read_unaligned() };
+                    let src_addr = Ipv4Addr::from(data.ipv4_address);
+                    info!("LOG: DST {}, ACTION {}", src_addr, data.action);
+                }
+            }
+        });
+    }
+
+
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
